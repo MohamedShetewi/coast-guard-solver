aa21b959bc4918dd4326e0501a93915aa6226542
Mohamed Shetewi <Muhamedshetewi@gmail.com>
2022-11-22 19:37:40 +0200

    Moved Station constructor to the top of the class

--- src/main/Entity/Station.java
+++ src/main/Entity/Station.java
@@ -5,16 +5,15 @@ public class Station {
     private Location location;
     private int passengersCount;
 
+    public Station(Location location) {
+        this.location = location;
+        this.passengersCount = 0;
+    }
     public Location getLocation() {
         return location;
     }
 
     public int getPassengersCount() {
         return passengersCount;
-    }
-
-    public Station(Location location) {
-        this.location = location;
-        this.passengersCount = 0;
     }
 }
