d7e6b7283345d17ac46d6d951c930900987612a2
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2022-11-23 17:02:42 +0200

    Modified heuristic choice mechanism.

--- src/main/Search/AStar.java
+++ src/main/Search/AStar.java
@@ -1,24 +1,11 @@
 package main.Search;
 
-import main.Problem.State;
-
 public class AStar extends InformedSearch {
 
     public AStar(int whichHeuristic) {
-        searchQueue = new InformedSearchQueue(((o1, o2) -> whichHeuristic == 1 ?
-                ((heuristic1(o1.getState()) + o1.getPathCostFromRoot()) - (heuristic1(o2.getState()) + o2.getPathCostFromRoot()))
-                : ((heuristic2(o1.getState()) + o1.getPathCostFromRoot()) - (heuristic2(o2.getState()) + o2.getPathCostFromRoot()))));
+        searchQueue = new InformedSearchQueue(((o1, o2) ->
+                ((heuristics[whichHeuristic].calculate(o1.getState()) + o1.getPathCostFromRoot())
+                        - (heuristics[whichHeuristic].calculate(o2.getState()) + o2.getPathCostFromRoot()))));
     }
 
-    @Override
-    public int heuristic1(State s) {
-        //TODO
-        return 0;
-    }
-
-    @Override
-    public int heuristic2(State s) {
-        //TODO
-        return 0;
-    }
 }
