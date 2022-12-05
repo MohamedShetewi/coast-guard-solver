e69ba7b4e5bff690b202cc00aa7471bf0d744257
Mohamed Shetewi <Muhamedshetewi@gmail.com>
2022-12-06 00:15:43 +0200

    Changed location from x,y -> i,j :))

--- src/code/Problem/Move.java
+++ src/code/Problem/Move.java
@@ -16,8 +16,8 @@ public class Move extends Operator {
         CoastGuardState newState = (CoastGuardState) oldState.clone();
         CoastGuardBoat coastGuardBoat = newState.getCoastGuardBoat();
 
-        int xLocation = coastGuardBoat.getLocation().getX();
-        int yLocation = coastGuardBoat.getLocation().getY();
+        int xLocation = coastGuardBoat.getLocation().getI();
+        int yLocation = coastGuardBoat.getLocation().getJ();
 
         switch (direction) {
             case UP:
@@ -49,8 +49,8 @@ public class Move extends Operator {
         int gridHeight = oldState.getGridHeight();
         int gridWidth = oldState.getGridWidth();
 
-        int xLocation = coastGuardBoat.getLocation().getX();
-        int yLocation = coastGuardBoat.getLocation().getY();
+        int xLocation = coastGuardBoat.getLocation().getI();
+        int yLocation = coastGuardBoat.getLocation().getJ();
 
         switch (direction) {
             case UP:
